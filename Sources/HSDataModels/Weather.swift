cc960462f1a5912377d8b5af4ba04481045f796f
Alexander Belyavskiy <diejmon@gmail.com>
2019-02-10 15:35:52 +0300

    Add public init to Weather

--- Sources/HSDataModels/Weather.swift
+++ Sources/HSDataModels/Weather.swift
@@ -4,4 +4,12 @@ public struct Weather: Codable, Equatable {
   public let date: Date
   public let temperature: Measurement<UnitTemperature>?
   public let humidity: Percentage?
+
+  public init(date: Date,
+              temperature: Measurement<UnitTemperature>? = nil,
+              humidity: Percentage? = nil) {
+    self.date = date
+    self.temperature = temperature
+    self.humidity = humidity
+  }
 }
