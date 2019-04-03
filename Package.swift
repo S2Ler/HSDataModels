b3a9892ddaec3b4c741a7a0779ccd2e5ef106ffc
Alexander Belyavskiy <diejmon@gmail.com>
2019-04-03 20:40:29 +0300

    Swift 5

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
-    name: "HSDataModels",
-    products: [
-        .library(
-            name: "HSDataModels",
-            targets: ["HSDataModels"]),
-    ],
-    dependencies: [
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "HSDataModels",
-            dependencies: []),
-        .testTarget(
-            name: "HSDataModelsTests",
-            dependencies: ["HSDataModels"]),
-    ],
-    swiftLanguageVersions: [.v4_2]
+  name: "HSDataModels",
+  platforms: [
+    .macOS(.v10_14)
+  ],
+  products: [
+    .library(
+      name: "HSDataModels",
+      targets: ["HSDataModels"]),
+  ],
+  targets: [
+    .target(
+      name: "HSDataModels",
+      dependencies: []),
+    .testTarget(
+      name: "HSDataModelsTests",
+      dependencies: ["HSDataModels"]),
+  ],
+  swiftLanguageVersions: [.v5]
 )
